3aeb2b35be6641ee47e3af07a765e96270e07a6f
Deeksha <dgondlya@pnw.edu>
2025-02-15 18:46:54 +0000

    Formatted code,Fixed warnings,Ran tests

--- CICD_Core/Calculator.cs
+++ CICD_Core/Calculator.cs
@@ -10,7 +10,7 @@ namespace CICD_Core
             {
                 throw new DivideByZeroException();
             }
-            return (double)v1 / v2;
+            return (double) v1 / v2;
         }
     }
 }
