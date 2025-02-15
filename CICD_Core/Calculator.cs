68e40c8b708fc19170bf91e61f75af7aa5d799c2
Deeksha <dgondlya@pnw.edu>
2025-02-15 21:31:33 +0000

    Removed unsued variable a

--- CICD_Core/Calculator.cs
+++ CICD_Core/Calculator.cs
@@ -5,7 +5,7 @@ namespace CICD_Core
     {
         public static double Divide(int v1, int v2)
         {
-            int a = 3;
+            
             if (v2 == 0)
             {
                 throw new DivideByZeroException();
